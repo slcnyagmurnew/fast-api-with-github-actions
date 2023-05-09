9fe7f3c7ebbb14045669cef35a501d99ae96f178
Selcan Yağmur Atak <l1119601@std.yildiz.edu.tr>
2023-05-09 12:02:15 +0300

    feat(tests): add class with asynctest

--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -1,12 +1,35 @@
-from application import say_hello
+import uvicorn
+from multiprocessing import Process
+import asyncio
+import aiohttp
+from application import Application
+import asynctest
 import unittest
 
 
-class TestApi(unittest.TestCase):
-    def test_say_hello(self):
+class TestApi(asynctest.TestCase):
+
+    async def setUp(self):
+        app = Application()
+        self.proc = Process(target=uvicorn.run,
+                            args=(app.api,),
+                            kwargs={
+                                "host": "127.0.0.1",
+                                "port": 8000,
+                                "log_level": "info"},
+                            daemon=True)
+        self.proc.start()
+        await asyncio.sleep(0.1)  # time for the server to start
+
+    async def tearDown(self):
+        self.proc.terminate()
+
+    async def test_say_hello(self):
         name = "Ahmet"
-        real_value = say_hello(name=name)
-        self.assertEqual(real_value, {"message": f"Hello {name}"})
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"http://127.0.0.1:8000/hello/{name}") as response:
+                returned = await response.json()
+        self.assertEqual(returned, {"message": f"Hello {name}"})
 
 
 if __name__ == "__main__":
