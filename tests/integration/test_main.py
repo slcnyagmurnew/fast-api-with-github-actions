ad468046e7f4c76d05675fd710920c3b5e9f2a3a
Selcan Yağmur Atak <l1119601@std.yildiz.edu.tr>
2023-05-09 11:15:17 +0300

    test(tests): add class definition

--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -2,10 +2,11 @@ from application import say_hello
 import unittest
 
 
-def test_say_hello():
-    name = "Ahmet"
-    real_value = say_hello(name=name)
-    assert real_value == {"message": f"Hello {name}"}
+class TestApi(unittest.TestCase):
+    def test_say_hello(self):
+        name = "Ahmet"
+        real_value = say_hello(name=name)
+        assert real_value == {"message": f"Hello {name}"}
 
 
 if __name__ == "__main__":
