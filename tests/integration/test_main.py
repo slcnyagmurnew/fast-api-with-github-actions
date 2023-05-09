418b5d25136755971f98cab2b717be559a9e807f
Selcan Yağmur Atak <l1119601@std.yildiz.edu.tr>
2023-05-09 11:21:15 +0300

    test(tests): add class definition

--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -6,7 +6,7 @@ class TestApi(unittest.TestCase):
     def test_say_hello(self):
         name = "Ahmet"
         real_value = say_hello(name=name)
-        assert real_value == {"message": f"Hello {name}"}
+        self.assertEqual(real_value == {"message": f"Hello {name}"})
 
 
 if __name__ == "__main__":
